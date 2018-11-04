1fb1b8ccf2b0d135704d750fc404e216f814b8df
ipelaezitec <i.pelaez@itecriocuarto.org.ar>
2018-11-04 20:57:13 -0300

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,72 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        
+        <div class="col-sm-7">
+            <div class="row mt-4 ">
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                            <div class="carousel-inner">
+                              <div class="carousel-item active">
+                                <img class="d-block w-100" src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1535256396516x634409788530319900%2Flarge.20180728020246_1.jpg.8250ed43c66c1f6282da4b76d31a1c58.jpg?w=768&h=431&auto=compress&fit=max" alt="First slide">
+                              </div>
+                              <div class="carousel-item">
+                                <img class="d-block w-100" src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1535256489897x761541015002876500%2Flarge.20180727041442_1.jpg.b54c3468fff407b67acb40f0872fe79b.jpg?w=768&h=431&auto=compress&fit=max" alt="Second slide">
+                              </div>
+                             
+                              <div class="carousel-item">
+                                <img class="d-block w-100" src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1535256600713x531589473830536000%2F20180724000142_1.jpg?w=768&h=431&auto=compress&fit=max" alt="Four slide">
+                              </div>
+                            </div>
+                          </div>
+            </div>
+           
+            <div class="row mt-4 video-row">
+                    
+                    <div class="embed-responsive embed-responsive-16by9">
+                        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/fB8TyLTD7EE"allowfullscreen></iframe>
+                    </div>
+            </div>
+        </div>
+        <div class="col">
+                   <div class="custom-card p-2 mt-4">
+                       <div class="text-card p-4">
+                            Requirements:
+                            • You must be 16+ to apply.
+                            • You must have a working microphone.
+                            • You must have a Legal Copy of Grand Theft Auto V
+                            • Please take time and effort in your application.
+                            Before you Apply:
+                            We strongly recommend you to take the time and effort required to be here. Applications with little to no effort will be denied!
+                            About us:
+                            SunbeltRP is a community based on civilian life roleplay with extensively trained emergency services. We aim to give the best experience to all members. Here at Sunbelt RP were Not just a Community, but a Family. Where everyone has a Chance to Make a Difference.
+                       </div>
+                       <button class="mx-auto btn appli-button">
+                           Apply today!
+                       </button>
+ 
+                   </div>
+        </div>
+    
+    </div>    
+</div>
+@endsection
+
+{{-- <div class="row justify-content-center">
+    <div class="col-md-8">
+        <div class="card">
+            <div class="card-header">Dashboard</div>
+
+            <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+
+                You are logged in!
+            </div>
+        </div>
+    </div>
+</div> --}}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'SBRP') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Raleway|Poppins" rel="stylesheet" type="text/css">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/mystyle.css') }}" rel="stylesheet">
+</head>
+<body class="background">
+    <div id="app">
+        {{-- <header class="custom-header align-items-center "> --}}
+        <header class="custom-header d-flex justify-content-between px-4 align-items-center ">
+        
+            {{-- <div class="container-fluid d-flex align-items-center  justify-content-between"> --}}
+                {{-- <div class="d-flex justify-content-between"> --}}
+                    <div class="d-flex p-2">
+                        {{-- <a href="{{ url('/') }}" class="m-1">
+                            <button class="general-button">SBRP Website</button>
+                        </a> --}}
+                        <a href="" class="m-1">
+                            <button class="general-button">Home</button>
+                        </a>
+                        <a href="" class="m-1">
+                            <button class="general-button">Contact</button>
+                        </a>
+                    </div>  
+                    
+                    <div class="d-flex">
+                        <img src="https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fs3.amazonaws.com%2Fappforest_uf%2Ff1535255119052x357929023215547200%2FSunbelt-Logo-cad.png?w=192&h=&auto=compress&fit=max" alt="" class="logo-center mx-auto">
+                    </div>  
+                    
+                    <div class="d-flex">
+                            @guest
+                            <a href="" class="m-1">
+                                <button class="general-button">Login</button>
+                            </a>
+                            <a href="" class="m-1">
+                                <button class="general-button">Sing Up</button>
+                            </a>
+                        @else
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+        
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+        
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        @endguest
+                    
+                    </div>  
+
+                {{-- </div> --}}
+
+
+
+
+
+                
+            </div>
+        
+        </header>
+        <div class="line"></div>
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
