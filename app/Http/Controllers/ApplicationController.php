2448888dc84b3c8ae51b831ef2149c677a889297
ipelaezitec <i.pelaez@itecriocuarto.org.ar>
2018-11-04 23:42:31 -0300

    application route and blade add
    still without form

--- app/Http/Controllers/ApplicationController.php
+++ app/Http/Controllers/ApplicationController.php
@@ -13,7 +13,7 @@ class ApplicationController extends Controller
      */
     public function index()
     {
-        
+        return view('application.application');
         //
     }
 
--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,4 @@ Auth::routes();
 // Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/','HomeController@index');
-// Route::get('/application','HomeController@index');
+Route::get('/application','ApplicationController@index');
